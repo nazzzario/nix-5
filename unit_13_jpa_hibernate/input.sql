2df0db5d3a2976ed6858f4859d2ad791d65392ec
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-07-25 22:11:09 +0300

    add fix

--- unit_13_jpa_hibernate/input.sql
+++ unit_13_jpa_hibernate/input.sql
@@ -1,28 +1,36 @@
-insert into corses(name)
-values ('Java Enterprise'),
-       ('Python Beginner');
+insert into courses (course_name)
+values ('Java'),
+       ('HTML'),
+       ('C++');
 
 insert into groups (group_name, course_id)
-values ('CS 231', 1),
-       ('AF 101', 2);
+values ('Java 101', 1),
+       ('HTML 101', 2),
+       ('C++ 101', 3);
 
-insert into students(first_name, last_name, group_id)
-values ('Mary', 'Potter', 1),
-       ('Jhon', 'Cena', 1),
-       ('Igor', 'Pink', 1),
-       ('Harry', 'Potter', 2),
-       ('Alex', 'Mayson', 2),
-       ('Victor', 'Morozov', 2);
+INSERT INTO students (email, first_name, last_name, group_id)
+VALUES ('sem@quispedeSuspendisse.net', 'Macon', 'Walters', 2),
+       ('Mauris.ut.quam@feugiatplaceratvelit.co.uk', 'Clarke', 'Rivas', 1),
+       ('velit.eu@lectusconvallisest.co.uk', 'Athena', 'Mcneil', 3),
+       ('facilisis.vitae.orci@seddolor.co.uk', 'Abra', 'Harrison', 1),
+       ('facilisis.facilisis.magna@elementumsemvitae.ca', 'Bruno', 'Howell', 3),
+       ('fermentum.arcu.Vestibulum@nibhDonecest.org', 'Jasper', 'Mccarthy', 1),
+       ('purus.accumsan@Quisque.co.uk', 'Ashely', 'Benjamin', 2),
+       ('leo@massarutrummagna.com', 'Michelle', 'Goff', 2),
+       ('fringilla@et.com', 'Francesca', 'Burke', 1),
+       ('velit.Cras.lorem@velmauris.net', 'Micah', 'Rosario', 1);
 
-insert into teachers(email, first_name, last_name, course_id)
-values ('robert_martin@mail.com', 'Robert', 'Martin', 1),
-       ('ivan@mail.com', 'Ivan', 'Ivanov', 2);
+INSERT INTO teachers (email, first_name, last_name)
+VALUES ('lorem.vitae@inceptoshymenaeosMauris.edu', 'Constance', 'Ortiz'),
+       ('neque@lorem.ca', 'Camille', 'Becker'),
+       ('consequat@sem.edu', 'Jada', 'Hahn');
 
-insert into topics (topic_name, course_id, teacher_id)
-values ('Spring Basics', 1, 1),
-       ('Python History', 2, 2);
+INSERT INTO topics (topic_name, teacher_id)
+VALUES ('leo, in lobortis tellus justo', 1),
+       ('Sed nulla ante, iaculis nec,', 2),
+       ('Cras eu tellus eu augue', 3);
 
-insert into lessons (date_time, name, topic_id)
-values ('2021-07-26 14:00:00', 'Starting spring configuration', 1),
-       ('2021-07-30 15:00:00', 'Python code conversion', 2);
-
+INSERT INTO lessons (date_time, name, topic_id, group_id)
+VALUES ('2021-12-26 19:40:44', 'Mauris', 1, 1),
+       ('2021-01-08 06:32:53', 'dolor', 2, 2),
+       ('2021-03-19 12:17:35', 'non,', 3, 3);
--- unit_13_jpa_hibernate/src/main/java/com/nkrasnovoronka/Main.java
+++ unit_13_jpa_hibernate/src/main/java/com/nkrasnovoronka/Main.java
@@ -3,6 +3,9 @@ package com.nkrasnovoronka;
 import com.nkrasnovoronka.util.HibernateSessionFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +13,10 @@ public class Main {
         try (Session session = sessionFactory.openSession()) {
             try {
                 session.beginTransaction();
-
+                Query query = session.createQuery("select l from Lesson l join Group g on l.group.id = g.id join Student s on s.group.id = g.id where s.id = :id");
+                query.setParameter("id", 1L);
+                List resultList = query.getResultList();
+                System.out.println(resultList);
 
                 session.getTransaction().commit();
             }catch (Exception e){
